fd6813c5d7d7d22ca629beb096cdff435429dd2d
Arnold Jiang <aj9183@princeton.edu>
2023-02-14 17:03:49 -0500

    edited replace.c

--- replace.c
+++ replace.c
@@ -37,7 +37,7 @@ static size_t replaceAndWrite(const char *pcLine,
    while (*pcLine != '\0') {
       /*points to the beginning of pcFrom every iteration*/
       pcTmp = pcFrom;
-      const char *pc = Str_search(pcLine, pcFrom); 
+      char *pc = Str_search(pcLine, pcFrom); 
       /* prints until pc */
       while (*pcLine != *pc) {
          putchar(*pcLine);
