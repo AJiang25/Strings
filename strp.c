d86a5b1def0e21a52c1005ddd2bb7b5ce8b8d01d
Arnold Jiang <aj9183@princeton.edu>
2023-02-15 16:01:03 -0500

    moved if statement in Str_search in strp.c

--- strp.c
+++ strp.c
@@ -76,9 +76,12 @@ char *Str_search(const char *pcHaystack, const char *pcNeedle) {
    while (*pc != '\0') {
       const char *pcTmp = pc; /* points to pc */
       const char *pcNeed = pcNeedle; /* points to pcNeedle */
-
       if (*pcNeedle == '\0') {
          return (char*) pc;
+      }
+      while (*pcNeed == *pcTmp && *pcNeed != '\0' && *pcTmp != '\0') {
+         pcNeed++;
+         pcTmp++;
       }
       pc++;
    }
