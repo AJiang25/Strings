819c0c4b20b19a4a1deebcdae99086ceb7204dff
Arnold Jiang <aj9183@princeton.edu>
2023-02-28 22:23:04 -0500

    fixed some comments

--- str.h
+++ str.h
@@ -21,12 +21,14 @@ to pcDest. */
 char *Str_concat(char pcDest[], const char pcSrc[]);
 
 /* Compares string Str1 with string Str2. Returns 0 if there is no
-difference, else returns an int value other than 0 */
+difference, else returns an size_t value other than 0 if there is
+a difference in the two strings*/
 size_t Str_compare(const char Str2[], const char Str1[]);
 
 /* Searches for string pcNeedle in string pcHaystack. Returns a 
-char pointer to pcHaystack if pcNeedle length is 0, an index in 
-pcHaystack if pcNeedle exists in pcHaystack, else returns NULL. */
+char pointer to pcHaystack if pcNeedle length is 0, or a char 
+pointer to the index in  pcHaystack if pcNeedle exists in 
+pcHaystack, else returns NULL. */
 char *Str_search(const char pcHaystack[], const char pcNeedle[]);
 
 #endif
